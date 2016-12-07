f63e5503e8302cd917e3bc47684583dee6399cef
Гоголев Сергей <gogolev.sergey@gmail.com>
2016-12-07 19:22:16 +0300

    init

--- chipsite/urls.py
+++ chipsite/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""chipsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('chip.urls'))
 ]
