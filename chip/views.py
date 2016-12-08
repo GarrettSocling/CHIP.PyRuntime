2429247d4c27052ff867dbc787d8ab2d81ee35e5
Гоголев Сергей <gogolev.sergey@gmail.com>
2016-12-08 08:59:23 +0300

    battery.sh, view template

--- chip/views.py
+++ chip/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
+class BatteryInfo():
+    def __init__(self, voltage,discharge):
+        self.voltage = voltage
+        self.discharge = discharge
+
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the chip index.")
+    volts = 'Error'
+    discharge = 'Error'
+    import os
+    from subprocess import Popen, PIPE
+    if os.name=="nt":
+        script = "battery.bat"
+    else:
+        script = "/usr/bin/battery.sh"
+    process = Popen(script, stdout=PIPE,shell=True)
+    (output, err) = process.communicate()
+    exit_code = process.wait()
+    output = output.decode("utf-8")
+    for kv in output.split(os.linesep):
+        keyval = kv.split("=")
+        if len(keyval)==2:
+            if keyval[0].strip() == 'Battery voltage':
+                volts = keyval[1].strip()
+            if keyval[0].strip() == 'Battery discharge current':
+                discharge = keyval[1].strip()
+    bat = BatteryInfo(volts,discharge)
+    template = loader.get_template('chip/index.html')
+    context = {
+        'bat': bat,
+    }
+    return HttpResponse(template.render(context, request))
--- chipsite/settings.py
+++ chipsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['192.168.1.6','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'chip.apps.ChipConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
