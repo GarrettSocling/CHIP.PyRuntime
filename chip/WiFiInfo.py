e2aae20dc423e886e3e8c3c77e821df0da7325c7
Гоголев Сергей <gogolev.sergey@gmail.com>
2017-05-03 22:02:59 +0300

    initial list of networks

--- chip/WiFiInfo.py
+++ chip/WiFiInfo.py
@@ -1,23 +1,31 @@
-""""""
+"""Get wifi info"""
 import os
 from subprocess import Popen, PIPE
 
 class WiFi():
-    """"""
+    """Info about wifi"""
     def __init__(self):
         self.ip = 'n/a'
+        self.nets = []
         if os.name == "nt":
             self.script = "wifi.bat"
         else:
             self.script = "./wifi.sh"
     def getInfo(self):
-        """"""
-        pass
+        """Get info from bash script"""
         process = Popen(self.script, stdout=PIPE, shell=True)
         (output, err) = process.communicate()
         exit_code = process.wait()
         output = output.decode("utf-8")
         lines = output.split(os.linesep)
-        for kval in output.split(os.linesep):
-            pass
-        self.ip=lines[0].strip()
+        self.ip = lines[0].strip()
+        for i in range(2,len(lines)-3):
+            self.nets.append(WifiNet(lines[i]))
+
+class WifiNet():
+    """Network info"""
+    def __init__(self,name):
+        self.name = name
+        self.signal=""
+        self.rate=""
+        self.security=""
--- wifi.bat
+++ wifi.bat
@@ -1,2 +1,8 @@
 @echo off
 echo 127.0.0.1
+
+echo *  SSID           MODE   CHAN  RATE       SIGNAL  BARS  SECURITY
+echo   MTSRouter-кв5  Infra  13    54 Mbit/s  70      ▂▄▆_  WPA2
+echo *  sinosoft.ru    Infra  1     54 Mbit/s  100     ▂▄▆█  WPA1 WPA2
+echo ""
+echo *  SSID  MODE  CHAN  RATE  SIGNAL  BARS  SECURITY
